f590e369149a7ae9c2fc5ca16d3752b83862ec79
ovidiu <enescu_vd@yahoo.com>
2019-01-15 22:32:56 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { RatesComponent } from './rates/rates.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpensesComponent,
+    RatesComponent,
+    CategoriesComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule
