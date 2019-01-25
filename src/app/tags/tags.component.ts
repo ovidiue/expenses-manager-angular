4a2b3be3cdae1fed8c3cf554983efac0f035b7c0
ovidiu <enescu_vd@yahoo.com>
2019-01-25 12:51:28 +0200

    added tag listing ui

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Tag} from '../classes/tag';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {TagService} from '../tag.service';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
-  styleUrls: ['./tags.component.sass']
+  styleUrls: ['./tags.component.scss'],
+  providers: [ConfirmationService, MessageService]
 })
 export class TagsComponent implements OnInit {
+  categories: Tag[] = [];
+  selectedCat: Tag[] = [];
 
-  constructor() { }
+  constructor(private tagService: TagService,
+              private confirmationService: ConfirmationService,
+              private messageService: MessageService) {
+  }
 
   ngOnInit() {
   }
