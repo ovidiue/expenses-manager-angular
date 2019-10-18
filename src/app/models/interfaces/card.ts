62af57b77a76dc4b1cfeb8067dfbe09436a7c237
ovidiu <enescu_vd@yahoo.com>
2019-10-19 00:54:38 +0300

    Add Card interface used in dashboard stats for categories and tags

---
+++ src/app/models/interfaces/card.ts
@@ -0,0 +1,12 @@
+export interface Card {
+  name: string;
+  color: string;
+  payed: number;
+  total: number;
+  min: number;
+  max: number;
+  totalRecurrent: number;
+  nonRecurrent: number;
+  closed: number;
+  noOfExpenses: number;
+}
