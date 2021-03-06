69cb28ddd2ade8f698a056b4f9add13c0b21335b
ovidiu <enescu.ovidiu@gmail.com>
2021-03-06 16:31:35 +0200

    change button selector

--- src/app/shared/components/button/button/button.component.ts
+++ src/app/shared/components/button/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: '[app-button]',
+  selector: '[appButton]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
