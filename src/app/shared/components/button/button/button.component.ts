09335cdc859c5e63e2861a4b7d315dfe67ca469b
ovidiu <enescu.ovidiu@gmail.com>
2021-03-06 16:56:26 +0200

    change button implementation

--- src/app/shared/components/button/button/button.component.ts
+++ src/app/shared/components/button/button/button.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: '[appButton]',
+  selector: 'button[appButton], a[appButton]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'app-button',
+    '[class.app-button--main]': `bType === 'main'`,
+    '[class.app-button--secondary]': `bType === 'secondary'`,
+    '[class.app-button--warning]': `bType === 'warning'`,
+  },
 })
-export class ButtonComponent  {
-
-  @Input() text = '';
+export class ButtonComponent {
   @Input() bType: 'main' | 'secondary' | 'warning' = 'main';
-
 }
